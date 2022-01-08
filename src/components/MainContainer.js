5d7a6c893c0c38260bbe6f7943f9adb9292cd537
ArnabDey1543 <arnabdey1543@gmail.com>
2022-01-08 20:32:06 +0530

    MainContainer, RightMenu created

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React,{useEffect} from "react";
+import { FaUser } from "react-icons/fa";
 import "../styles/MainContainer.css";
+import { AudioList } from "./AudioList";
 import { Banner } from "./Banner";
 
 function MainContainer() {
+
+
+    useEffect(() => {
+        const allLi = document
+          .querySelector(".menuList")
+          .querySelectorAll("li");
+    
+        function changeMenuActive() {
+          allLi.forEach((n) => n.classList.remove("active"));
+          this.classList.add("active");
+        }
+    
+        allLi.forEach((n) => n.addEventListener("click", changeMenuActive));
+      }, []);
+
   return (
     <div className="mainContainer">
       <Banner />
+
+      <div className="menuList">
+        <ul>
+          <li>
+            <a href="#">Popular</a>
+          </li>
+          <li>
+            <a href="#">Albums</a>
+          </li>
+          <li>
+            <a href="#">Songs</a>
+          </li>
+          <li>
+            <a href="#">Fans</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+        </ul>
+        <p><i><FaUser /></i>12.3M <span>Followers</span></p>
+      </div>
+
+      <AudioList />
     </div>
   );
 }
--- src/components/RightMenu.js
+++ src/components/RightMenu.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
 import "../styles/RightMenu.css";
+import { FaCrown, FaRegHeart, FaBell, FaSun, FaCogs } from "react-icons/fa";
+import Profile from '../img/profile.jpg';
+
 function RightMenu() {
-    return (
-        <div className="rightMenu">
-            
+  return (
+    <div className="rightMenu">
+      <div className="goPro">
+        <i>
+          <FaCrown />
+          <p>
+            Go <span>Pro</span>
+          </p>
+        </i>
+        <i>
+          <FaBell />
+        </i>
+        <i>
+          <FaRegHeart />
+        </i>
+      </div>
+      <div className="profile">
+        <i>
+          <FaSun />
+        </i>
+        <i>
+          <FaCogs />
+        </i>
+        <div className="profileMage">
+            <img src={Profile} alt="" />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export { RightMenu };
