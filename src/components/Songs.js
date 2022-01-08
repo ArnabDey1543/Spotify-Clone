fa8a9f6288d1930860f6db8bdf912f5154c81f46
ArnabDey1543 <arnabdey1543@gmail.com>
2022-01-08 20:42:06 +0530

    Song list numbering

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -54,42 +54,7 @@ const Songs = [
     imgSrc:
       "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fheymama.jpg?alt=media&token=2d8e8632-8051-4105-9760-3c715d797802",
   },
-  {
-    id: 7,
-    favourite: false,
-    songName: "Charlie",
-    artist: "Gopi Sundar",
-    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FCharlie.mp3?alt=media&token=5a129114-bb31-4a5a-95b7-1767829df21e",
-    imgSrc:
-      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fcharlie.jpg?alt=media&token=81a1dc08-a9af-4db2-b74d-1f50315f9342",
-  },
-  {
-    id: 8,
-    favourite: false,
-    songName: "Naanga Vera Maari",
-    artist: "Yuvan Shankar Raja",
-    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2Fvalimai.mp3?alt=media&token=97c769ab-e2fb-4562-90aa-18c73b320bde",
-    imgSrc:
-      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fvalimai.jpg?alt=media&token=e11b839d-2514-4e1a-aa3a-20db3540a837",
-  },
-  {
-    id: 9,
-    favourite: false,
-    songName: "Beast",
-    artist: "Anirudh Ravichander",
-    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2Fbeast.mp3?alt=media&token=ebf29a01-079b-44eb-969a-57ee66a15ee8",
-    imgSrc:
-      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fbeast.jpg?alt=media&token=7cc7c33e-8eac-4f7a-858d-75e75d4b609c",
-  },
-  {
-    id: 10,
-    favourite: false,
-    songName: "Turkish Folk Battle Song",
-    artist: "CVR Toon ",
-    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FPlevne-turkish.mp3?alt=media&token=72f7e03b-2865-4ffb-a2d1-da4c4557fb80",
-    imgSrc:
-      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fturkish.jpg?alt=media&token=1f44ad17-43a8-4e1d-b232-a78b28cb9fe0",
-  },
+
 ];
 
 export { Songs };
